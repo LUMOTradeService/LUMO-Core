bfba6613adc1cb0e6fafc825fed913c9471d9ae5
LUMO_L0ki <39536753+LUMOL0ki@users.noreply.github.com>
2020-08-05 10:47:19 +0200

    Create Calculator.cs

---
+++ LUMO.Core/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LUMO.Core
+{
+    public class Calculator
+    {
+        public static double Addition(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subrtaction(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Division(double a, double b)
+        {
+            return a / b;
+        }
+
+        public static double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+    }
+}
