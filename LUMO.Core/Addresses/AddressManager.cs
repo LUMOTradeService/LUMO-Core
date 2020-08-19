5d4c2428be291e39e59e0fb5fd97959a53b72495
LUMO_L0ki <39536753+LUMOL0ki@users.noreply.github.com>
2020-08-19 13:17:30 +0200

    AddressManager

--- LUMO.Core/Addresses/AddressManager.cs
+++ LUMO.Core/Addresses/AddressManager.cs
@@ -1,4 +1,5 @@
 ﻿using LUMO.Addresses;
+using LUMO.Data.Addresses;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +8,7 @@ namespace LUMO.Core.Addresses
 {
     public class AddressManager
     {
-        private AddressManager()
+        private AddressManager(IAddressStore addressStore)
         {
 
         }
